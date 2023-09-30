c15a40232c02dbbaee2d97836c000d3c1f8a42f8
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2023-09-30 11:57:55 -0300

    Modifica o conteúdo do dataProjects e atualiza a estilização do content-component

--- src/app/data/dataProjects.ts
+++ src/app/data/dataProjects.ts
@@ -2,19 +2,19 @@ export const dataProjects = [
     {
         "id":"1",
         "photo":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
-        "title":"bbbbbbb",
-        "description":"cccccc"
+        "title":"Pokedex",
+        "description":"Um projeto utilizando a PokeApi para listar Pokemons e algumas de suas características. Feito durante o Bootcamp Santander 2023 pela DIO. Tecnologias utilizadas: HTML, CSS e Javascript"
     },
     {
         "id":"2",
         "photo":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
-        "title":"bbbbbbb",
-        "description":"cccccc"
+        "title":"Vanflix",
+        "description":"Um projeto inspirado em plataformas de streaming, onde foi utilizado uma api de filmes e séries. Feito durante o módulo 2 do curso Desenvolvedor(a) Front-End do Vai na Web. Tecnologia utilizada: React JS"
     },
     {
         "id":"3",
         "photo":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
-        "title":"bbbbbbb",
-        "description":"cccccc"
+        "title":"Crespos",
+        "description":"Um projeto visual de uma página voltada para cabelos crespos. Feito durante o módulo 1 do curso Desenvolvedor(a) Front-End do Vai na Web. Tecnologias utilizadas: HTML e CSS."
     }
 ]
